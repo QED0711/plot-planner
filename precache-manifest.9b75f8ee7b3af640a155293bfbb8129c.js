fb87b42a1e508515e734f0268df4dd3d0a316231
Quinn Dizon <qdizon@gmail.com>
2019-06-27 10:26:43 -0400

    Updates

--- precache-manifest.9b75f8ee7b3af640a155293bfbb8129c.js
+++ precache-manifest.9b75f8ee7b3af640a155293bfbb8129c.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b304e07841a32b15c293882e35302bed",
+    "revision": "8190c8753e5266e1b4e452def169b422",
     "url": "/plot-planner/index.html"
   },
   {
-    "revision": "36f57b0382aa2365cb23",
+    "revision": "f5688d749d63ad16c9b6",
     "url": "/plot-planner/static/css/main.dfa2557a.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/plot-planner/static/js/2.7471cb3b.chunk.js"
   },
   {
-    "revision": "36f57b0382aa2365cb23",
-    "url": "/plot-planner/static/js/main.aa39424b.chunk.js"
+    "revision": "f5688d749d63ad16c9b6",
+    "url": "/plot-planner/static/js/main.7f5a2066.chunk.js"
   },
   {
     "revision": "fa7e246eb073a3d5e94a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/plot-planner/precache-manifest.74d750cacfb92c3156862f8cbf103aa2.js"
+  "/plot-planner/precache-manifest.9b75f8ee7b3af640a155293bfbb8129c.js"
 );
 
 self.addEventListener('message', (event) => {
